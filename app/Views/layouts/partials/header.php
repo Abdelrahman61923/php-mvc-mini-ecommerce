082d085015bffccf232330409c9a8e625fcfb75c
Abdelrahman61923 <abdoelrhman1@gmail.com>
2026-02-03 16:59:39 +0200

    php-mvc-mini-ecommerce

--- app/Views/layouts/partials/header.php
+++ app/Views/layouts/partials/header.php
@@ -38,14 +38,63 @@
         </div>
         <div class="header-right ms-auto">
             <div class="d-flex align-items-center">
+                <div class="nxl-h-item d-none d-sm-flex">
+                    <div class="full-screen-switcher">
+                        <a href="javascript:void(0);" class="nxl-head-link me-0" onclick="$('body').fullScreenHelper('toggle');">
+                            <i class="feather-maximize maximize"></i>
+                            <i class="feather-minimize minimize"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="nxl-h-item dark-light-theme">
+                    <a href="javascript:void(0);" class="nxl-head-link me-0 dark-button">
+                        <i class="feather-moon"></i>
+                    </a>
+                    <a href="javascript:void(0);" class="nxl-head-link me-0 light-button" style="display: none">
+                        <i class="feather-sun"></i>
+                    </a>
+                </div>
+                <div class="dropdown nxl-h-item">
+                    <a class="nxl-head-link me-3" data-bs-toggle="dropdown" href="#" role="button" data-bs-auto-close="outside">
+                        <i class="feather-bell"></i>
+                        <span class="badge bg-danger nxl-h-badge">1</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end nxl-h-dropdown nxl-notifications-menu">
+                        <div class="d-flex justify-content-between align-items-center notifications-head">
+                            <h6 class="fw-bold text-dark mb-0">Notifications</h6>
+                            <a href="javascript:void(0);" class="fs-11 text-success text-end ms-auto" data-bs-toggle="tooltip" title="Make as Read">
+                                <i class="feather-check"></i>
+                                <span>Make as Read</span>
+                            </a>
+                        </div>
+                        <div class="notifications-item">
+                            <img src="/php/mini-ecommerce/public/assets/images/avatar/2.png" alt="" class="rounded me-3 border" />
+                            <div class="notifications-desc">
+                                <a href="javascript:void(0);" class="font-body text-truncate-2-line"> <span class="fw-semibold text-dark">Malanie Hanvey</span> We should talk about that at lunch!</a>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="notifications-date text-muted border-bottom border-bottom-dashed">2 minutes ago</div>
+                                    <div class="d-flex align-items-center float-end gap-2">
+                                        <a href="javascript:void(0);" class="d-block wd-8 ht-8 rounded-circle bg-gray-300" data-bs-toggle="tooltip" title="Make as Read"></a>
+                                        <a href="javascript:void(0);" class="text-danger" data-bs-toggle="tooltip" title="Remove">
+                                            <i class="feather-x fs-12"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-center notifications-footer">
+                            <a href="javascript:void(0);" class="fs-13 fw-semibold text-dark">Alls Notifications</a>
+                        </div>
+                    </div>
+                </div>
                 <div class="dropdown nxl-h-item">
                     <a href="javascript:void(0);" data-bs-toggle="dropdown" role="button" data-bs-auto-close="outside">
-                        <img src="/php/mini-ecommerce/public/assets/images/avatar/1.png" alt="user-image" class="img-fluid user-avtar me-0" />
+                        <img src="/php/mini-ecommerce/public/assets/images/avatar/13.jpg" alt="user-image" class="img-fluid user-avtar me-0" />
                     </a>
                     <div class="dropdown-menu dropdown-menu-end nxl-h-dropdown nxl-user-dropdown">
                         <div class="dropdown-header">
                             <div class="d-flex align-items-center">
-                                <img src="/php/mini-ecommerce/public/assets/images/avatar/1.png" alt="user-image" class="img-fluid user-avtar" />
+                                <img src="/php/mini-ecommerce/public/assets/images/avatar/13.jpg" alt="user-image" class="img-fluid user-avtar" />
                                 <div>
                                     <h6 class="text-dark mb-0"><?= $_SESSION['user_name'] ?></h6>
                                     <span class="fs-12 fw-medium text-muted"><?= $_SESSION['user_email'] ?></span>
